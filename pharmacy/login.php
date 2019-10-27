c842763a2bc3d4c54bb95c814fd70b5eb7628042
Achsuthan <achsuthan@icloud.com>
2019-10-27 10:29:20 +0530

    1. Pharmacy Login API added 2. Pharmacy Register API added
    >>> ach

--- pharmacy/login.php
+++ pharmacy/login.php
@@ -2,10 +2,23 @@
 require_once('../common/header.php');
 require_once('../common/connection.php');
 require_once('./pharmacy.php');
+require_once('../common/messageHandler.php');
 $connection = new connection();
 $conn = $connection->getConnection();
-
 $pharmacy = new pharmacy($conn);
-$pharmacy->login();
+
+$data = json_decode(file_get_contents("php://input"));
+
+
+if(!empty($data->email) && !empty($data->password)){
+    $pharmacy->pharmacy_email = $data->email;
+    $pharmacy->pharmacy_password = $data->password;
+    echo $pharmacy->login();
+}
+else {
+    $messageHandler = new messageHandler('failed', 400, 'Email and Password required', 'User login failed');
+    echo $messageHandler->getResponse();
+}
+
 
 
