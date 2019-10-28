8642e5ea449823016d1102f1890f6bf54038963c
Achsuthan <achsuthan@icloud.com>
2019-10-28 23:29:56 +0530

    1. Get prescription for delivered api added
    >>> ach

---
+++ prescription/getPrescriptionForDelivered.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('../common/header.php');
+require_once('../common/connection.php');
+require_once('./prescription.php');
+require_once('../common/messageHandler.php');
+
+$connection = new connection();
+$conn = $connection->getConnection();
+
+$prescription = new prescription($conn);
+
+$data = json_decode(file_get_contents("php://input"));
+if (!empty($data->pharmacyId)) {
+    $prescription->pharmacyId = $data->pharmacyId;
+    $prescription->getPrescriptionForDelivered();
+} else {
+    $messageHandler = new messageHandler('failed', 400, 'All filed required', 'All field required');
+    echo $messageHandler->getResponse();
+}
