757863b48e8a1122b784f81f521f28ccb1e74d1b
Achsuthan <achsuthan@icloud.com>
2019-10-28 11:46:34 +0530

    1. New modification added 2. Add New functions added
    >>> ach

--- prescription/prescription.php
+++ prescription/prescription.php
@@ -1,22 +1,25 @@
 <?php
 require_once("../common/messageHandler.php");
 require_once("../tablet/tablet.php");
-class prescription{
+class prescription
+{
     private $conn;
     private $tableName = "prescription";
     public $userId;
     public $doctorId;
     public $tablets = [];
     private $prescriptionId;
+    public $pre_name;
 
     public function __construct($db)
     {
         $this->conn = $db;
     }
 
-    function addPrescription(){
-        if(count($this->tablets) > 0){
-            $sql = "INSERT INTO " . $this->tableName . " (user_id, doctor_id) VALUES ('".$this->userId."', '".$this->doctorId."')";
+    function addPrescription()
+    {
+        if (count($this->tablets) > 0) {
+            $sql = "INSERT INTO " . $this->tableName . " (user_id, doctor_id, pre_name) VALUES ('" . $this->userId . "', '" . $this->doctorId . "', '" . $this->pre_name . "')";
             if ($this->conn->query($sql) === TRUE) {
                 $this->prescriptionId = $this->conn->insert_id;
                 $this->addTablets();
@@ -24,17 +27,38 @@ class prescription{
                 $messageHandler = new messageHandler("faild", 500, "Tablet add faild", "Server Failed");
                 echo $messageHandler->getResponse();
             }
-        }
-        else {
+        } else {
             $messageHandler = new messageHandler('failed', 400, 'All filed required', 'Tablets required');
             echo $messageHandler->getResponse();
         }
     }
 
-    function addTablets(){
+    function addTablets()
+    {
         $tablets = new tablet($this->conn);
         $tablets->tablets = $this->tablets;
         $tablets->prescriptionId = $this->prescriptionId;
         $tablets->addTablets();
+    }
+
+    function getPrescriptionForPharmacy()
+    {
+        $tablets = new tablet($this->conn);
+        $arr = $tablets->getPrescriptionForPharmacy();
+        if (count($arr) > 0) {
+            $tArr = array();
+            foreach ($arr as $ar) {
+                $query = "SELECT * FROM " . $this->tableName . " WHERE id = '$ar'";
+                $stmt = $this->conn->query($query);
+                while ($row = $stmt->fetch_assoc()) {
+                    array_push($tArr, array("id" => $row["id"], "pre_name" => $row["pre_name"], "user_id"=> $row["user_id"]));
+                }
+            }
+            $tArr = array("status" => 200, "details" => "prescription found", "details" => $tArr);
+            echo json_encode($tArr);
+        } else {
+            $messageHandler = new messageHandler('failed', 400, 'All filed required', array());
+            echo $messageHandler->getResponse();
+        }
     }
 }
